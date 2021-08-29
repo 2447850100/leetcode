0141b97299ae857e232e2d8688fbe22bdded4997
2447850100 <hu19981027>
2021-08-29 14:34:54 +0800

    no message
    Signed-off-by: 2447850100 <hu19981027>

--- leetcode/src/com/xiaohu/leetcode/ClimbStairs.java
+++ leetcode/src/com/xiaohu/leetcode/ClimbStairs.java
@@ -6,7 +6,7 @@ package com.xiaohu.leetcode;
  */
 public class ClimbStairs {
     public static void main(String[] args) {
-        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(5));
     }
 
     public static int climbStairs(int n) {
